fc68a5d58470af3de4f1328b67445da9de2981cf
Srikanth Tangella <srikanth.tangella@gmail.com>
2025-08-07 21:15:07 -0500

    Basic MVVM Example
    1. Create a Model
    2. Pass the Model to ViewModel
    3. Pass the ViewModel to View where its displayed
    Nothing Fancy

---
+++ BasicMVVM/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicMVVM
+{
+    /// <summary>
+    /// Interaction logic for CarWindow.xaml
+    /// </summary>
+    public partial class CarWindow : Window
+    {
+        public CarWindow()
+        {
+            InitializeComponent();
+
+            // Create a sample car and its view model
+            Car car = new Car("Toyota", "Corolla", 2020);
+            CarViewModel carViewModel = new CarViewModel(car);
+            DataContext = carViewModel;
+        }
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+    }
+
+    public class CarViewModel
+    {
+        public Car Car { get; set; }
+        public CarViewModel(Car car)
+        {
+            Car = car;
+        }
+    }
+}
